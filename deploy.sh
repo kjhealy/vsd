629351e93b53c5487df7c29512c6d765f6ea192c
Kieran Healy <kjhealy@gmail.com>
2022-12-31 20:32:19 -0500

    Updates; first live test

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-# VSD deploy
+## VSD deploy
+## Requires if (Sys.info()["user"] == "kjhealy") to be TRUE in _targets.R
 PUBLIC_DIR='_site/'
 SSH_USER='kjhealy@kjhealy.co'
 DOCUMENT_ROOT='~/public/visualizingsociety.com/public_html'
 
 echo "Uploading changes to remote server..."
-echo "Dry run: (Not doing anything for real)"
-echo "rsync --dry-run --exclude='.DS_Store' -Prvzce 'ssh -p 22'" $PUBLIC_DIR $SSH_USER:$DOCUMENT_ROOT "--delete-after"
-#rsync --dry-run --exclude='.DS_Store' -Prvzce 'ssh -p 22' $PUBLIC_DIR $SSH_USER:$DOCUMENT_ROOT --delete-after
+cp html/htaccess $PUBLIC_DIR/.htaccess
+rsync --exclude='.DS_Store' -Prvzce 'ssh -p 22' $PUBLIC_DIR $SSH_USER:$DOCUMENT_ROOT --delete-after
